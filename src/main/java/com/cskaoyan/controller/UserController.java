0fc82f54b2ea3114301b467af123d212b987c894
pengshun <1229232647@qq.com>
2019-05-17 16:12:50 +0800

    可以打开登录页面

--- src/main/java/com/cskaoyan/controller/UserController.java
+++ src/main/java/com/cskaoyan/controller/UserController.java
@@ -1,4 +1,17 @@
 package com.cskaoyan.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
 public class UserController {
+
+
+    @RequestMapping("")
+    public String init() {
+
+        return "login";
+    }
 }
