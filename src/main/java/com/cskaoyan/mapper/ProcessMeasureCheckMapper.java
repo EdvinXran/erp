45b87f152fdd59d9d9d89866f621bd457d3417c5
zhaoyoujia <601795728@qq.com>
2019-05-17 18:02:19 +0800

    增加逆向文件

---
+++ src/main/java/com/cskaoyan/mapper/ProcessMeasureCheckMapper.java
@@ -0,0 +1,31 @@
+package com.cskaoyan.mapper;
+
+import com.cskaoyan.bean.ProcessMeasureCheck;
+import com.cskaoyan.bean.ProcessMeasureCheckExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface ProcessMeasureCheckMapper {
+    long countByExample(ProcessMeasureCheckExample example);
+
+    int deleteByExample(ProcessMeasureCheckExample example);
+
+    int deleteByPrimaryKey(String pMeasureCheckId);
+
+    int insert(ProcessMeasureCheck record);
+
+    int insertSelective(ProcessMeasureCheck record);
+
+    List<ProcessMeasureCheck> selectByExample(ProcessMeasureCheckExample example);
+
+    ProcessMeasureCheck selectByPrimaryKey(String pMeasureCheckId);
+
+    int updateByExampleSelective(@Param("record") ProcessMeasureCheck record, @Param("example") ProcessMeasureCheckExample example);
+
+    int updateByExample(@Param("record") ProcessMeasureCheck record, @Param("example") ProcessMeasureCheckExample example);
+
+    int updateByPrimaryKeySelective(ProcessMeasureCheck record);
+
+    int updateByPrimaryKey(ProcessMeasureCheck record);
+}
