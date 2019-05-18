4c7f5bd976efdf6b22c305d3bd05da091c307f8f
zhaoyoujia <601795728@qq.com>
2019-05-18 17:41:36 +0800

    不合格产品管理增删改查1

--- src/main/java/com/cskaoyan/controller/UnQualifyController.java
+++ src/main/java/com/cskaoyan/controller/UnQualifyController.java
@@ -2,6 +2,7 @@ package com.cskaoyan.controller;
 
 import com.cskaoyan.bean.QualityManageVo;
 import com.cskaoyan.bean.UnqualifyApply;
+import com.cskaoyan.bean.UpdateVo;
 import com.cskaoyan.service.UnQualifyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,10 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -23,26 +23,55 @@ public class UnQualifyController {
     UnQualifyService unQualifyService;
 
     /**
-     * 不合格品申请查询分发
+     * 跳转到显示页面
      */
     @RequestMapping("/find")
-    String qualityManage(){
+    public String qualityManage(HttpServletRequest request){
+
+        HttpSession session = request.getSession();
+        List<String> per = new ArrayList<>();
+        per.add("unqualify:add");
+        per.add("unqualify:edit");
+        per.add("unqualify:delete");
+        session.setAttribute("sysPermissionList",per);
         return "unqualify_list";
     }
+
     /**
      * 分页查询不合格申请记录
      *
      * @return
      */
     @RequestMapping("/list")
-    public @ResponseBody
-    QualityManageVo findPartUnqualify(@RequestParam int page, @RequestParam int rows) {
+    @ResponseBody
+    public QualityManageVo findPartUnqualify(@RequestParam int page, @RequestParam int rows) {
         QualityManageVo qualityManageVo = new QualityManageVo();
-        List<UnqualifyApply> unqualifyList = unQualifyService.findAll();
+        List<UnqualifyApply> unqualifyList = unQualifyService.findAll(page, rows);
         qualityManageVo.setRows(unqualifyList);
         qualityManageVo.setTotal(unqualifyList == null ? 0 : unqualifyList.size());
         System.out.println(qualityManageVo);
         return qualityManageVo;
+    }
+
+
+    /**
+     * 添加之前的判断
+     */
+    @RequestMapping("/add_judge")
+    @ResponseBody
+    public String addJudge(){
+
+        return null;
+    }
+
+    /**
+     * 加载添加子页面
+     */
+
+    @RequestMapping("/add")
+    public String loadAdd(){
+
+        return "unqualify_add";
     }
 
     /**
@@ -50,10 +79,34 @@ public class UnQualifyController {
      *
      * @return
      */
-    @RequestMapping("/add_batch")
-    public String addUnqualify() {
+    @RequestMapping("/insert")
+    @ResponseBody
+    public UpdateVo insertUnqualify(@RequestParam UnqualifyApply unqualifyApply) {
+        UpdateVo updateVo = new UpdateVo();
+        unQualifyService.insertUnqualify(unqualifyApply);
+        // 模拟数据
+        updateVo.setData(null);
+        updateVo.setMsg(null);
+        updateVo.setStatus(null);
+        return updateVo;
+    }
 
-        return "unqualify_list";
+    /**
+     * 编辑之前的判断
+     */
+    @RequestMapping("/edit_judge")
+    public String editJudge(){
+
+        return null;
+    }
+
+    /**
+     * 加载编辑子页面
+     */
+    @RequestMapping("/edit")
+    public String loadEdit(){
+
+        return "unqualify_edit";
     }
 
     /**
@@ -61,8 +114,23 @@ public class UnQualifyController {
      *
      * @return
      */
-    @RequestMapping("/edit_batch")
-    public String editUnqualify() {
+    @RequestMapping("/update_all")
+    @ResponseBody
+    public UpdateVo updateUnqualify(@RequestParam UnqualifyApply unqualifyApply) {
+        UpdateVo updateVo = new UpdateVo();
+        unQualifyService.updateUnqualify(unqualifyApply);
+        // 模拟数据
+        updateVo.setData(null);
+        updateVo.setMsg(null);
+        updateVo.setStatus(null);
+        return updateVo;
+    }
+
+    /**
+     * 删除之前的判断
+     */
+    @RequestMapping("/delete_judge")
+    public String deleteJudge(){
 
         return "unqualify_list";
     }
@@ -73,9 +141,15 @@ public class UnQualifyController {
      * @return
      */
     @RequestMapping("/delete_batch")
-    public String delUnqualify() {
-
-        return "unqualify_list";
+    @ResponseBody
+    public UpdateVo delUnqualify(@RequestParam List<String> ids) {
+        UpdateVo updateVo = new UpdateVo();
+        unQualifyService.delUnqualify(ids);
+        // 模拟数据
+        updateVo.setData(null);
+        updateVo.setMsg(null);
+        updateVo.setStatus(null);
+        return updateVo;
     }
 
 
--- src/main/java/com/cskaoyan/service/impl/UnQualifyServiceImpl.java
+++ src/main/java/com/cskaoyan/service/impl/UnQualifyServiceImpl.java
@@ -4,6 +4,7 @@ import com.cskaoyan.bean.UnqualifyApply;
 import com.cskaoyan.bean.UnqualifyApplyExample;
 import com.cskaoyan.mapper.UnqualifyApplyMapper;
 import com.cskaoyan.service.UnQualifyService;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,13 +17,36 @@ public class UnQualifyServiceImpl implements UnQualifyService {
     private UnqualifyApplyMapper unqualifyApplyMapper;
 
     /**
+     * 添加不合格申请记录
+     * @param unqualifyApply 需要添加的项
+     */
+    @Override
+    public void insertUnqualify(UnqualifyApply unqualifyApply) {
+        int insert = unqualifyApplyMapper.insert(unqualifyApply);
+    }
+
+    @Override
+    public void updateUnqualify(UnqualifyApply unqualifyApply) {
+        UnqualifyApplyExample uqae = new UnqualifyApplyExample();
+        int i = unqualifyApplyMapper.updateByExample(unqualifyApply, uqae);
+    }
+
+    /**
      * 显示不合格申请记录
      * @return 显示记录列表
      */
-    public List<UnqualifyApply> findAll(){
+    public List<UnqualifyApply> findAll(int page, int rows){
         UnqualifyApplyExample uqae = new UnqualifyApplyExample();
-       // PageHelper.startPage(page, rows);
+        PageHelper.startPage(page, rows);
         List<UnqualifyApply> applyList = unqualifyApplyMapper.selectByExample(uqae);
         return applyList;
+    }
+
+    @Override
+    public void delUnqualify(List<String> ids) {
+        UnqualifyApplyExample uqae = new UnqualifyApplyExample();
+        UnqualifyApplyExample.Criteria criteria = uqae.createCriteria();
+        criteria.andUnqualifyApplyIdIn(ids);
+        int i = unqualifyApplyMapper.deleteByExample(uqae);
     }
 }
