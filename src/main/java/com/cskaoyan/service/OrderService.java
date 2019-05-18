e2f7a4c524dc6b5c48b9882d93bbb5b8bb00288c
pengshun <1229232647@qq.com>
2019-05-18 09:52:27 +0800

    hehe

--- src/main/java/com/cskaoyan/service/OrderService.java
+++ src/main/java/com/cskaoyan/service/OrderService.java
@@ -1,10 +1,11 @@
 package com.cskaoyan.service;
 
 import com.cskaoyan.bean.Order;
+import com.github.pagehelper.PageInfo;
 
 import java.util.List;
 
 public interface OrderService {
 
-    List<Order> getOrders(String page, String rows);
+    PageInfo<Order> getOrders(String page, String rows);
 }
