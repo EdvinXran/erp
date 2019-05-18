334b44792505e75d1a854f0a855860dca06bba40
GYP <ebob@>
2019-05-18 18:03:01 +0800

    ....

---
+++ src/main/java/com/cskaoyan/mapper/Material_receiveMapper.java
@@ -0,0 +1,31 @@
+package com.cskaoyan.mapper;
+
+import com.cskaoyan.bean.Material_receive;
+import com.cskaoyan.bean.Material_receiveExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface Material_receiveMapper {
+    long countByExample(Material_receiveExample example);
+
+    int deleteByExample(Material_receiveExample example);
+
+    int deleteByPrimaryKey(String receiveId);
+
+    int insert(Material_receive record);
+
+    int insertSelective(Material_receive record);
+
+    List<Material_receive> selectByExample(Material_receiveExample example);
+
+    Material_receive selectByPrimaryKey(String receiveId);
+
+    int updateByExampleSelective(@Param("record") Material_receive record, @Param("example") Material_receiveExample example);
+
+    int updateByExample(@Param("record") Material_receive record, @Param("example") Material_receiveExample example);
+
+    int updateByPrimaryKeySelective(Material_receive record);
+
+    int updateByPrimaryKey(Material_receive record);
+}
